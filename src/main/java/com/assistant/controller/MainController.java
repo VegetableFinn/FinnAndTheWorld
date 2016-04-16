c7ade3cb25a28f6073d1b4f0aa43f6d2da7efb8e
VegetableFinn <vGdh6NEA6iruuvQK>
2016-04-16 19:12:51 +0800

    #get rid of all ones

--- src/main/java/com/assistant/controller/MainController.java
+++ src/main/java/com/assistant/controller/MainController.java
@@ -1,15 +1,8 @@
 package com.assistant.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.assistant.db.model.One;
-import com.assistant.service.OneService;
 
 /**
  * @author hefan.hf
@@ -18,17 +11,14 @@ import com.assistant.service.OneService;
 @Controller
 public class MainController {
 
-    @Autowired
-    private OneService oneService;
-
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
         return "index";
     }
 
-    @RequestMapping("/showInfos")
-    public @ResponseBody Object showUserInfos() {
-        List<One> userInfos = oneService.getAll();
-        return userInfos;
-    }
+    //    @RequestMapping("/showInfos")
+    //    public @ResponseBody Object showUserInfos() {
+    //        List<One> userInfos = oneService.getAll();
+    //        return userInfos;
+    //    }
 }
